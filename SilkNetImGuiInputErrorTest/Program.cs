56e1e1a31462b52e852f09c41a0c2ea4a96e7e28
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2024-11-30 20:54:05 +0900

    init

---
+++ SilkNetImGuiInputErrorTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+
+namespace Tutorial
+{
+    class Program
+    {
+        private static IWindow _window;
+        private static ImGuiController _controller;
+
+        private static void Main(string[] args)
+        {
+            //Create a window.
+            var options = WindowOptions.Default;
+            options.Size = new Vector2D<int>(800, 600);
+            options.Title = "LearnOpenGL with Silk.NET";
+
+            _window = Window.Create(options);
+
+            //Assign events.
+            _window.Load += OnLoad;
+            _window.Update += OnUpdate;
+            _window.Render += OnRender;
+            _window.FramebufferResize += OnFramebufferResize;
+
+            //Run the window.
+            _window.Run();
+
+            // window.Run() is a BLOCKING method - this means that it will halt execution of any code in the current
+            // method until the window has finished running. Therefore, this dispose method will not be called until you
+            // close the window.
+            _window.Dispose();
+        }
+
+
+        private static void OnLoad()
+        {
+            //Set-up input context.
+            IInputContext input = _window.CreateInput();
+            for (int i = 0; i < input.Keyboards.Count; i++)
+            {
+                input.Keyboards[i].KeyDown += KeyDown;
+            }
+
+            var gl = _window.CreateOpenGL();
+
+            _controller = new ImGuiController(gl, _window, input, () => { });
+        }
+
+        private static void OnRender(double obj)
+        {
+            //Here all rendering should be done.
+        }
+
+        private static void OnUpdate(double obj)
+        {
+            //Here all updates to the program should be done.
+        }
+
+        private static void OnFramebufferResize(Vector2D<int> newSize)
+        {
+            //Update aspect ratios, clipping regions, viewports, etc.
+        }
+
+        private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
+        {
+            Console.WriteLine($"{arg2} {arg3}");
+            //Check to close the window on escape.
+            if (arg2 == Key.Escape)
+            {
+                _window.Close();
+            }
+        }
+    }
+}
